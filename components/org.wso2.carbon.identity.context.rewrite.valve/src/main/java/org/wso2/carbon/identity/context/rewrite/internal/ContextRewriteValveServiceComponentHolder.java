72221c7577772cf550e23c7d9a9c66411b1dcbdf
emsw.bandara <emsw.bandara@gmail.com>
2017-07-04 12:08:11 +0530

    fixing issues

--- components/org.wso2.carbon.identity.context.rewrite.valve/src/main/java/org/wso2/carbon/identity/context/rewrite/internal/ContextRewriteValveServiceComponentHolder.java
+++ components/org.wso2.carbon.identity.context.rewrite.valve/src/main/java/org/wso2/carbon/identity/context/rewrite/internal/ContextRewriteValveServiceComponentHolder.java
@@ -21,7 +21,6 @@ package org.wso2.carbon.identity.context.rewrite.internal;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.user.core.service.RealmService;
-import sun.plugin.dom.exception.InvalidStateException;
 
 public class ContextRewriteValveServiceComponentHolder {
 
