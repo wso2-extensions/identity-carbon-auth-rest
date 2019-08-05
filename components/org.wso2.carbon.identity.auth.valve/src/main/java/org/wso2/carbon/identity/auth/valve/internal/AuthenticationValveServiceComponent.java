225c1aed94c3a4284b89eb0fbd2d5d53d75c09a9
senthalan <senthalank@gmail.com>
2019-08-05 23:13:54 +0530

    Change the scr doc comment to component annotation

--- components/org.wso2.carbon.identity.auth.valve/src/main/java/org/wso2/carbon/identity/auth/valve/internal/AuthenticationValveServiceComponent.java
+++ components/org.wso2.carbon.identity.auth.valve/src/main/java/org/wso2/carbon/identity/auth/valve/internal/AuthenticationValveServiceComponent.java
@@ -15,7 +15,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.wso2.carbon.identity.auth.valve.internal;
 
 import org.apache.commons.logging.Log;
@@ -24,68 +23,70 @@ import org.osgi.service.component.ComponentContext;
 import org.wso2.carbon.identity.auth.service.AuthenticationManager;
 import org.wso2.carbon.identity.auth.service.factory.AuthenticationRequestBuilderFactory;
 import org.wso2.carbon.identity.core.handler.HandlerComparator;
-
 import java.util.List;
-
 import static java.util.Collections.sort;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 
-/**
- * @scr.component name="org.wso2.carbon.identity.auth.valve" immediate="true"
- * @scr.reference name="org.wso2.carbon.identity.auth.service.manager"
- * interface="org.wso2.carbon.identity.auth.service.AuthenticationManager"
- * cardinality="1..1" policy="dynamic" bind="setAuthenticationManager" unbind="unsetAuthenticationManager"
- * @scr.reference name="org.wso2.carbon.identity.auth.service.factory.auth"
- * interface="org.wso2.carbon.identity.auth.service.factory.AuthenticationRequestBuilderFactory"
- * cardinality="0..n" policy="dynamic" bind="addAuthenticationRequestBuilderFactory"
- * unbind="removeAuthenticationRequestBuilderFactory"
- */
-
+@Component(
+         name = "org.wso2.carbon.identity.auth.valve", 
+         immediate = true)
 public class AuthenticationValveServiceComponent {
 
     private static final Log log = LogFactory.getLog(AuthenticationValveServiceComponent.class);
 
+    @Activate
     protected void activate(ComponentContext cxt) {
         if (log.isDebugEnabled()) {
             log.debug("AuthenticationValveServiceComponent is activated");
         }
     }
 
+    @Reference(
+             name = "org.wso2.carbon.identity.auth.service.manager", 
+             service = org.wso2.carbon.identity.auth.service.AuthenticationManager.class, 
+             cardinality = ReferenceCardinality.MANDATORY, 
+             policy = ReferencePolicy.DYNAMIC, 
+             unbind = "unsetAuthenticationManager")
     protected void setAuthenticationManager(AuthenticationManager authenticationManager) {
         if (log.isDebugEnabled()) {
-            log.debug("Set AuthenticationManager, " + authenticationManager != null ? authenticationManager
-                    .getName() : " Unknown");
+            log.debug("Set AuthenticationManager, " + authenticationManager != null ? authenticationManager.getName() : " Unknown");
         }
-        List<AuthenticationManager> authenticationManagers = AuthenticationValveServiceHolder.getInstance()
-                .getAuthenticationManagers();
+        List<AuthenticationManager> authenticationManagers = AuthenticationValveServiceHolder.getInstance().getAuthenticationManagers();
         authenticationManagers.add(authenticationManager);
         sort(authenticationManagers, new HandlerComparator());
     }
 
     protected void unsetAuthenticationManager(AuthenticationManager authenticationManager) {
         if (log.isDebugEnabled()) {
-            log.debug("Unset AuthenticationManager, " + authenticationManager != null ? authenticationManager
-                    .getName() : " Unknown");
+            log.debug("Unset AuthenticationManager, " + authenticationManager != null ? authenticationManager.getName() : " Unknown");
         }
-        List<AuthenticationManager> authenticationManagers = AuthenticationValveServiceHolder.getInstance()
-                .getAuthenticationManagers();
+        List<AuthenticationManager> authenticationManagers = AuthenticationValveServiceHolder.getInstance().getAuthenticationManagers();
         authenticationManagers.remove(authenticationManager);
     }
 
+    @Reference(
+             name = "org.wso2.carbon.identity.auth.service.factory.auth", 
+             service = org.wso2.carbon.identity.auth.service.factory.AuthenticationRequestBuilderFactory.class, 
+             cardinality = ReferenceCardinality.MULTIPLE, 
+             policy = ReferencePolicy.DYNAMIC, 
+             unbind = "removeAuthenticationRequestBuilderFactory")
     protected void addAuthenticationRequestBuilderFactory(AuthenticationRequestBuilderFactory requestBuilderFactory) {
         if (log.isDebugEnabled()) {
-            log.debug(
-                    "Set AuthenticationRequestBuilderFactory, " + requestBuilderFactory != null ? requestBuilderFactory
-                            .getName() : "Unknown");
+            log.debug("Set AuthenticationRequestBuilderFactory, " + requestBuilderFactory != null ? requestBuilderFactory.getName() : "Unknown");
         }
         AuthenticationValveServiceHolder.getInstance().getRequestBuilderFactories().add(requestBuilderFactory);
     }
 
-    protected void removeAuthenticationRequestBuilderFactory(AuthenticationRequestBuilderFactory
-                                                                     requestBuilderFactory) {
+    protected void removeAuthenticationRequestBuilderFactory(AuthenticationRequestBuilderFactory requestBuilderFactory) {
         if (log.isDebugEnabled()) {
-            log.debug("Unset AuthenticationRequestBuilderFactory, " + requestBuilderFactory
-                      != null ? requestBuilderFactory.getName() : "Unknown");
+            log.debug("Unset AuthenticationRequestBuilderFactory, " + requestBuilderFactory != null ? requestBuilderFactory.getName() : "Unknown");
         }
         AuthenticationValveServiceHolder.getInstance().getRequestBuilderFactories().remove(requestBuilderFactory);
     }
 }
+
