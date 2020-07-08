bfab99d8a904f58bd06e674a1ed72ab6710c8423
Oshan Mudannayake <oshan.ivantha@gmail.com>
2020-07-08 07:59:30 +0530

    Add CORS Valve

---
+++ components/org.wso2.carbon.identity.cors.valve/src/main/java/org/wso2/carbon/identity/cors/valve/internal/CORSValveServiceComponent.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.cors.valve.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.wso2.carbon.identity.cors.service.CORSManager;
+
+/**
+ * Service component class for the CORS valve.
+ */
+@Component(name = "cors.valve", immediate = true)
+public class CORSValveServiceComponent {
+
+    private static final Log log = LogFactory.getLog(CORSValveServiceComponent.class);
+
+    /**
+     * Activate the CORSValveServiceComponent.
+     *
+     * @param context
+     */
+    @Activate
+    protected void activate(ComponentContext context) {
+
+        if (log.isDebugEnabled()) {
+            log.debug("CORSValveServiceComponent is activated.");
+        }
+    }
+
+    /**
+     * Deactivate the CORSServiceComponent.
+     *
+     * @param context
+     */
+    @Deactivate
+    protected void deactivate(ComponentContext context) {
+
+        if (log.isDebugEnabled()) {
+            log.debug("CORSServiceComponent is deactivated.");
+        }
+    }
+
+    /**
+     * Set the CORSManager.
+     *
+     * @param corsManager
+     */
+    @Reference(
+            name = "cors.service",
+            service = CORSManager.class,
+            cardinality = ReferenceCardinality.MANDATORY,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unsetCORSManager")
+    protected void setCorsManager(CORSManager corsManager) {
+
+        if (log.isDebugEnabled()) {
+            log.debug("Setting the CORSService.");
+        }
+        CORSValveServiceHolder.getInstance().setCorsManager(corsManager);
+    }
+
+    /**
+     * Unset the CORSManager.
+     *
+     * @param corsManager
+     */
+    protected void unsetCORSManager(CORSManager corsManager) {
+
+        if (log.isDebugEnabled()) {
+            log.debug("Unsetting the CORSService.");
+        }
+        CORSValveServiceHolder.getInstance().setCorsManager(null);
+    }
+}
---
+++ components/org.wso2.carbon.identity.cors.valve/src/main/java/org/wso2/carbon/identity/cors/valve/internal/util/RequestTagger.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ * NOTE: The code/logic in this class is copied from https://bitbucket.org/thetransactioncompany/cors-filter.
+ * All credits goes to the original authors of the project https://bitbucket.org/thetransactioncompany/cors-filter.
+ */
+
+package org.wso2.carbon.identity.cors.valve.internal.util;
+
+import org.wso2.carbon.identity.cors.valve.constant.Header;
+import org.wso2.carbon.identity.cors.valve.model.CORSRequestType;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Request tagger. Tags HTTP servlet requests to provide CORS information to downstream handlers.
+ */
+public final class RequestTagger {
+
+    /**
+     * Private constructor of RequestTagger.
+     */
+    private RequestTagger() {
+
+    }
+
+    /**
+     * Tags an HTTP servlet request to provide CORS information to downstream handlers.
+     * Tagging is provided via {@code HttpServletRequest.setAttribute()}.
+     * {@code cors.isCorsRequest} set to {@code true} or {@code false}.
+     * {@code cors.origin} set to the value of the "Origin" header, {@code null} if undefined.
+     * {@code cors.requestType} set to "actual" or "preflight" (for CORS requests).
+     * {@code cors.requestHeaders} set to the value of the "Access-Control-Request-Headers" or {@code null} if
+     * undefined (added for preflight CORS requests only).
+     *
+     * @param request The servlet request to inspect and tag. Must not be {@code null}.
+     * @param type    The detected request type. Must not be {@code null}.
+     */
+    public static void tag(final HttpServletRequest request, final CORSRequestType type) {
+
+        switch (type) {
+            case ACTUAL:
+                request.setAttribute("cors.isCorsRequest", true);
+                request.setAttribute("cors.origin", request.getHeader(Header.ORIGIN));
+                request.setAttribute("cors.requestType", "actual");
+                break;
+            case PREFLIGHT:
+                request.setAttribute("cors.isCorsRequest", true);
+                request.setAttribute("cors.origin", request.getHeader(Header.ORIGIN));
+                request.setAttribute("cors.requestType", "preflight");
+                request.setAttribute("cors.requestHeaders", request.getHeader(Header.ACCESS_CONTROL_REQUEST_HEADERS));
+                break;
+            case OTHER:
+                request.setAttribute("cors.isCorsRequest", false);
+        }
+    }
+}
