4cd6b3152dd3c81f8d08e49441e2f5b780e96d37
Oshan Mudannayake <oshan.ivantha@gmail.com>
2020-07-21 08:56:22 +0530

    Add CORS fixes

--- components/org.wso2.carbon.identity.cors.service/src/main/java/org/wso2/carbon/identity/cors/service/internal/store/ServerCORSStore.java
+++ components/org.wso2.carbon.identity.cors.service/src/main/java/org/wso2/carbon/identity/cors/service/internal/store/ServerCORSStore.java
@@ -18,19 +18,17 @@
 
 package org.wso2.carbon.identity.cors.service.internal.store;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.base.IdentityConstants;
-import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.identity.cors.mgt.core.exception.CORSManagementServiceClientException;
 import org.wso2.carbon.identity.cors.mgt.core.internal.util.CORSConfigurationUtils;
-import org.wso2.carbon.identity.cors.mgt.core.model.Origin;
 import org.wso2.carbon.identity.cors.mgt.core.model.ValidatedOrigin;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.wso2.carbon.identity.cors.service.constant.ErrorMessages.ERROR_CODE_INVALID_ORIGIN;
 
 /**
  * This class loads and persists the server level CORS origins.
@@ -39,30 +37,22 @@ public class ServerCORSStore {
 
     private static final Log log = LogFactory.getLog(ServerCORSStore.class);
 
-    private static ValidatedOrigin[] serverCORSOrigins;
+    private static List<ValidatedOrigin> serverCORSOrigins;
 
-    public static ValidatedOrigin[] getServerCORSOrigins() {
+    public static List<ValidatedOrigin> getServerCORSOrigins() {
 
         if (serverCORSOrigins == null) {
-            List<ValidatedOrigin> validatedOrigins = new ArrayList<>();
-
             // Get server level allowed CORS origins.
-            String allowedOriginsProperty = IdentityUtil.getProperty(IdentityConstants.CORS.ALLOWED_ORIGINS);
-            if (StringUtils.isNotBlank(allowedOriginsProperty) && !allowedOriginsProperty.equals("*")) {
-                for (Origin origin : CORSConfigurationUtils.parseWords(allowedOriginsProperty).stream().map(Origin::new)
-                        .collect(Collectors.toList())) {
-                    try {
-                        ValidatedOrigin validatedOrigin = new ValidatedOrigin(origin.getValue());
-                        validatedOrigins.add(validatedOrigin);
-                    } catch (CORSManagementServiceClientException e) {
-                        if (log.isDebugEnabled()) {
-                            log.debug(e);
-                        }
-                    }
-                }
-            }
 
-            serverCORSOrigins = validatedOrigins.toArray(new ValidatedOrigin[0]);
+            serverCORSOrigins = CORSConfigurationUtils.readPropertyArray(IdentityConstants.CORS.ALLOWED_ORIGINS)
+                    .stream().map(origin -> {
+                        try {
+                            return new ValidatedOrigin(origin);
+                        } catch (CORSManagementServiceClientException e) {
+                            log.error(String.format(ERROR_CODE_INVALID_ORIGIN.getDescription(), origin), e);
+                        }
+                        return null;
+                    }).collect(Collectors.toList());
         }
 
         return serverCORSOrigins;
