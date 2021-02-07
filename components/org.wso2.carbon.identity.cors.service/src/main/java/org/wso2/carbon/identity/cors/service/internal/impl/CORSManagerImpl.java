4df73c18de985565b14f124f693bb237f22d38d7
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2021-02-07 17:15:32 +0530

    Move CORSOrigin caches to the CORS Management Service

--- components/org.wso2.carbon.identity.cors.service/src/main/java/org/wso2/carbon/identity/cors/service/internal/impl/CORSManagerImpl.java
+++ components/org.wso2.carbon.identity.cors.service/src/main/java/org/wso2/carbon/identity/cors/service/internal/impl/CORSManagerImpl.java
@@ -20,10 +20,7 @@ package org.wso2.carbon.identity.cors.service.internal.impl;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
 import org.wso2.carbon.identity.cors.mgt.core.constant.ErrorMessages;
-import org.wso2.carbon.identity.cors.mgt.core.dao.CORSConfigurationDAO;
-import org.wso2.carbon.identity.cors.mgt.core.dao.CORSOriginDAO;
 import org.wso2.carbon.identity.cors.mgt.core.exception.CORSManagementServiceException;
 import org.wso2.carbon.identity.cors.mgt.core.exception.CORSManagementServiceServerException;
 import org.wso2.carbon.identity.cors.mgt.core.model.CORSConfiguration;
@@ -31,15 +28,10 @@ import org.wso2.carbon.identity.cors.mgt.core.model.CORSOrigin;
 import org.wso2.carbon.identity.cors.mgt.core.model.Origin;
 import org.wso2.carbon.identity.cors.service.CORSManager;
 import org.wso2.carbon.identity.cors.service.internal.CORSServiceHolder;
-import org.wso2.carbon.identity.cors.service.internal.cache.CORSConfigurationCache;
-import org.wso2.carbon.identity.cors.service.internal.cache.CORSConfigurationCacheEntry;
-import org.wso2.carbon.identity.cors.service.internal.cache.CORSConfigurationCacheKey;
-import org.wso2.carbon.identity.cors.service.internal.cache.CORSOriginCache;
-import org.wso2.carbon.identity.cors.service.internal.cache.CORSOriginCacheEntry;
-import org.wso2.carbon.identity.cors.service.internal.cache.CORSOriginCacheKey;
 import org.wso2.carbon.identity.cors.service.internal.function.CORSOriginToOrigin;
 import org.wso2.carbon.identity.cors.service.internal.store.ServerCORSStore;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -53,27 +45,20 @@ public class CORSManagerImpl implements CORSManager {
     @Override
     public Origin[] getCORSOrigins(String tenantDomain) throws CORSManagementServiceServerException {
 
-        Origin[] cachedResult = getCORSOriginsFromCache(tenantDomain);
-        if (cachedResult != null) {
-            return cachedResult;
-        }
-
+        List<Origin> originList = new ArrayList<>();
         try {
-            int tenantId = IdentityTenantUtil.getTenantId(tenantDomain);
-            List<CORSOrigin> corsOriginList = getCORSOriginDAO().getCORSOriginsByTenantId(tenantId);
+            // Get CORSOrigins of the server.
+            originList.addAll(ServerCORSStore.getServerCORSOrigins());
 
-            List<Origin> originList = ServerCORSStore.getServerCORSOrigins();
+            // Get CORSOrigins of the tenant.
+            List<CORSOrigin> corsOriginList  = CORSServiceHolder.getInstance().getCorsManagementService()
+                    .getTenantCORSOrigins(tenantDomain);
             if (!corsOriginList.isEmpty()) {
                 originList.addAll(corsOriginList.stream()
                         .map(new CORSOriginToOrigin()).collect(Collectors.toList()));
             }
 
-            Origin[] originArray = originList.toArray(new Origin[0]);
-
-            // Add to cache.
-            addCORSOriginsToCache(originArray, tenantDomain);
-
-            return originArray;
+            return originList.toArray(new Origin[0]);
         } catch (CORSManagementServiceException e) {
             throw new CORSManagementServiceServerException(ErrorMessages.ERROR_CODE_CORS_RETRIEVE.getCode(),
                     String.format(ErrorMessages.ERROR_CODE_CORS_RETRIEVE.getDescription(), tenantDomain), e);
@@ -83,136 +68,12 @@ public class CORSManagerImpl implements CORSManager {
     @Override
     public CORSConfiguration getCORSConfiguration(String tenantDomain) throws CORSManagementServiceException {
 
-        CORSConfiguration cachedResult = getCORSConfigurationFromCache(tenantDomain);
-        if (cachedResult != null) {
-            return cachedResult;
-        }
-
         try {
-            CORSConfiguration corsConfiguration = getCORSConfigurationDAO()
-                    .getCORSConfigurationByTenantDomain(tenantDomain);
-
-            // Add to cache.
-            addCORSConfigurationToCache(corsConfiguration, tenantDomain);
-
-            return corsConfiguration;
+            return CORSServiceHolder.getInstance().getCorsManagementService()
+                    .getCORSConfiguration(tenantDomain);
         } catch (CORSManagementServiceException e) {
             throw new CORSManagementServiceServerException(ErrorMessages.ERROR_CODE_CORS_CONFIG_RETRIEVE.getCode(),
                     String.format(ErrorMessages.ERROR_CODE_CORS_CONFIG_RETRIEVE.getDescription(), tenantDomain), e);
         }
-    }
-
-    /**
-     * Returns a CORSOriginDAO instance.
-     *
-     * @return A CORSOriginDAO instance.
-     */
-    private CORSOriginDAO getCORSOriginDAO() {
-
-        return CORSServiceHolder.getInstance().getCorsOriginDAO();
-    }
-
-    /**
-     * Returns a CORSConfigurationDAO instance.
-     *
-     * @return A CORSConfigurationDAO instance.
-     */
-    private CORSConfigurationDAO getCORSConfigurationDAO() {
-
-        return CORSServiceHolder.getInstance().getCorsConfigurationDAO();
-    }
-
-    /**
-     * Add CORS origins to the cache.
-     *
-     * @param origins      The  origins that should be added to the cache.
-     * @param tenantDomain The tenant domain specific to the cache entry.
-     */
-    private void addCORSOriginsToCache(Origin[] origins, String tenantDomain) {
-
-        CORSOriginCacheKey cacheKey = new CORSOriginCacheKey(tenantDomain);
-        CORSOriginCacheEntry cacheEntry = new CORSOriginCacheEntry(origins);
-
-        if (log.isDebugEnabled()) {
-            log.debug("Adding CORS origins to Cache with Key: " + tenantDomain);
-        }
-
-        CORSOriginCache.getInstance().addToCache(cacheKey, cacheEntry);
-    }
-
-    /**
-     * Get CORS origins from the cache.
-     *
-     * @param tenantDomain The tenant domain specific to the cache entry.
-     * @return Returns an array of {@code Origin}(s) if the cached origins are found for the tenant.
-     * Else return {@code null}.
-     */
-    private Origin[] getCORSOriginsFromCache(String tenantDomain) {
-
-        CORSOriginCacheKey cacheKey = new CORSOriginCacheKey(tenantDomain);
-        CORSOriginCache cache = CORSOriginCache.getInstance();
-        CORSOriginCacheEntry cacheEntry = cache.getValueFromCache(cacheKey);
-        if (cacheEntry == null) {
-            if (log.isDebugEnabled()) {
-                log.debug("Cache entry not found for cache key:" + tenantDomain);
-            }
-            return null;
-        }
-
-        if (cacheEntry.getValidatedOrigins() == null) {
-            if (log.isDebugEnabled()) {
-                log.debug("Could not find CORS origins in the cache entry.");
-            }
-            return null;
-        }
-
-        return cacheEntry.getValidatedOrigins();
-    }
-
-    /**
-     * Add CORS configurations to the cache.
-     *
-     * @param corsConfiguration The cors configuration that should be added to the cache.
-     * @param tenantDomain      The tenant domain specific to the cache entry.
-     */
-    private void addCORSConfigurationToCache(CORSConfiguration corsConfiguration, String tenantDomain) {
-
-        CORSConfigurationCacheKey cacheKey = new CORSConfigurationCacheKey(tenantDomain);
-        CORSConfigurationCacheEntry cacheEntry = new CORSConfigurationCacheEntry(corsConfiguration);
-
-        if (log.isDebugEnabled()) {
-            log.debug("Adding CORS configuration to Cache with Key: " + tenantDomain);
-        }
-
-        CORSConfigurationCache.getInstance().addToCache(cacheKey, cacheEntry);
-    }
-
-    /**
-     * Get CORS configuration from the cache.
-     *
-     * @param tenantDomain The tenant domain specific to the cache entry.
-     * @return Returns an instance of {@code CORSConfiguration}(s) if the cached CORS configuration is found for the
-     * tenant. Else return {@code null}.
-     */
-    private CORSConfiguration getCORSConfigurationFromCache(String tenantDomain) {
-
-        CORSConfigurationCacheKey cacheKey = new CORSConfigurationCacheKey(tenantDomain);
-        CORSConfigurationCache cache = CORSConfigurationCache.getInstance();
-        CORSConfigurationCacheEntry cacheEntry = cache.getValueFromCache(cacheKey);
-        if (cacheEntry == null) {
-            if (log.isDebugEnabled()) {
-                log.debug("Cache entry not found for cache key :" + tenantDomain);
-            }
-            return null;
-        }
-
-        if (cacheEntry.getCorsConfiguration() == null) {
-            if (log.isDebugEnabled()) {
-                log.debug("Could not find CORS configuration in the cache entry.");
-            }
-            return null;
-        }
-
-        return cacheEntry.getCorsConfiguration();
     }
 }
