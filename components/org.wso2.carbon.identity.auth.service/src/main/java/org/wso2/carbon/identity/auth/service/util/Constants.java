332861a6d8ba34ae7c57579e2b36115ad60b69e4
Thisara-Welmilla <thisara@wso2.com>
2021-07-14 16:22:44 +0530

    Refactored code.

--- components/org.wso2.carbon.identity.auth.service/src/main/java/org/wso2/carbon/identity/auth/service/util/Constants.java
+++ components/org.wso2.carbon.identity.auth.service/src/main/java/org/wso2/carbon/identity/auth/service/util/Constants.java
@@ -37,4 +37,6 @@ public class Constants {
     public final static String COOKIE_BASED_TOKEN_BINDING_EXT_PARAM = "atbv";
 
     public final static String CURRENT_SESSION_IDENTIFIER = "currentSessionIdentifier";
+
+    public final static String BASIC_CLIENT_AUTH_HANDLER = "BasicClientAuthenticationHandler";
 }
