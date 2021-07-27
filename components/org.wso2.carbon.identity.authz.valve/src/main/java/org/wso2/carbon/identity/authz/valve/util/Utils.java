35f5df4ff8095582e4dfc75c0b6903531544994b
Thisara-Welmilla <thisara@wso2.com>
2021-07-27 07:20:33 +0530

    Client credentials validation against Tenant

--- components/org.wso2.carbon.identity.authz.valve/src/main/java/org/wso2/carbon/identity/authz/valve/util/Utils.java
+++ components/org.wso2.carbon.identity.authz.valve/src/main/java/org/wso2/carbon/identity/authz/valve/util/Utils.java
@@ -21,6 +21,8 @@ package org.wso2.carbon.identity.authz.valve.util;
 import org.apache.catalina.connector.Request;
 import org.wso2.carbon.identity.application.common.model.User;
 import org.wso2.carbon.identity.auth.service.AuthenticationContext;
+import org.wso2.carbon.identity.oauth.dao.OAuthAppDO;
+import org.wso2.carbon.identity.oauth2.util.OAuth2Util;
 import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
 
 public class Utils {
@@ -52,7 +54,14 @@ public class Utils {
 
         String tenantDomainFromURLMapping = getTenantDomainFromURLMapping(request);
         User user = authenticationContext.getUser();
-        String userDomain = user.getTenantDomain();
-        return tenantDomainFromURLMapping.equals(userDomain);
+
+        String tenantDomain;
+        if (user != null) {
+            tenantDomain = user.getTenantDomain();
+        } else {
+            OAuthAppDO oAuthAppDO = (OAuthAppDO) authenticationContext.getProperties("oAuthAppDO");
+            tenantDomain = OAuth2Util.getTenantDomainOfOauthApp(oAuthAppDO);
+        }
+        return tenantDomainFromURLMapping.equals(tenantDomain);
     }
 }
