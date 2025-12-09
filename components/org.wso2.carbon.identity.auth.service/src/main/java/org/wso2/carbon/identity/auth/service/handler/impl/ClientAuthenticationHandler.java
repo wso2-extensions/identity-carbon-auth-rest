5df908cbf6f8e1b7dc09421f7a268179f8bdbd23
Maduranga Siriwardena <maduranga.siriwardena@gmail.com>
2025-12-09 09:35:35 +0530

    Add Java 21 compile time support

--- components/org.wso2.carbon.identity.auth.service/src/main/java/org/wso2/carbon/identity/auth/service/handler/impl/ClientAuthenticationHandler.java
+++ components/org.wso2.carbon.identity.auth.service/src/main/java/org/wso2/carbon/identity/auth/service/handler/impl/ClientAuthenticationHandler.java
@@ -23,6 +23,7 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.http.HttpHeaders;
+import org.osgi.annotation.bundle.Capability;
 import org.slf4j.MDC;
 import org.wso2.carbon.identity.auth.service.AuthenticationContext;
 import org.wso2.carbon.identity.auth.service.AuthenticationResult;
@@ -45,6 +46,13 @@ import static org.wso2.carbon.identity.auth.service.util.AuthConfigurationUtil.i
  * ClientAuthenticationHandler is for authenticate the request based on Client Authentication.
  * canHandle method will confirm whether this request can be handled by this authenticator or not.
  */
+@Capability(
+        namespace = "osgi.service",
+        attribute = {
+                "objectClass=org.wso2.carbon.identity.auth.service.handler.AuthenticationHandler",
+                "service.scope=singleton"
+        }
+)
 public class ClientAuthenticationHandler extends AuthenticationHandler {
 
     private static final Log log = LogFactory.getLog(ClientAuthenticationHandler.class);
